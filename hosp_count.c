6ae085885c4d0677dfe870dff22ee1e52944abd5
kabircsit <kabircsit25@oic.edu.np>
2026-04-10 18:44:47 +0545

    New branch hosp_count is created for counting the patient in each ward

--- hosp_count.c
+++ hosp_count.c
@@ -1,47 +1,161 @@
-#include<stdio.h>
-struct hosp_count
+
+#include <stdio.h>
+struct hosp_read
 {
-    char name[20];
+    char name[50];
     int age;
     char address[50];
-    char condition[100];
-    char ward[20];
+    char condition[50];
+    char ward_name[50];
 };
-int main(){
-    struct hosp_count hpp;
-    int i, w1=0, w2=0, w3=0, w4=0, w5=0, w6=0, w7=0, w8=0, w9=0, w10=0, count=0;
+int main()
+{
+    int i, w1 = 0, w2 = 0, w3 = 0, w4 = 0, w5 = 0, w6 = 0, w7 = 0, w8 = 0, w9 = 0, w10 = 0;
+    struct hosp_read hpp[24];
     FILE *fp;
-    fp=fopen("hospital.txt","r");
-    if(fp==NULL)
+    fp = fopen("hospital.txt", "r");
+    for (i = 0; i < 24; i++)
     {
-        printf("Error opening file!");
-        return 1;
-    }
-    while(!feof)
-    {
-        fscanf(fp,"%s %d %s %s %s",hpp.name,&hpp.age,hpp.address,hpp.condition,hpp.ward);
-        if(strcmp(hpp.ward,"")==0)
+        fscanf(fp, "%s %d %s %s %s", &hpp[i].name, &hpp[i].age, &hpp[i].address, &hpp[i].condition, &hpp[i].ward_name);
+        if (strcmp(hpp[i].ward_name, "GynoWard") == 0)
+        {
             w1++;
-        else if(strcmp(hpp.ward,"Ward2")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "OrthoWard") == 0)
+        {
             w2++;
-        else if(strcmp(hpp.ward,"Ward3")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "CardioWard") == 0)
+        {
             w3++;
-        else if(strcmp(hpp.ward,"Ward4")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "NeuroWard") == 0)
+        {
             w4++;
-        else if(strcmp(hpp.ward,"Ward5")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "PediatricWard") == 0)
+        {
             w5++;
-        else if(strcmp(hpp.ward,"Ward6")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "EmergencyWard") == 0)
+        {
             w6++;
-        else if(strcmp(hpp.ward,"Ward7")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "ICUWard") == 0)
+        {
             w7++;
-        else if(strcmp(hpp.ward,"Ward8")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "SurgicalWard") == 0)
+        {
             w8++;
-        else if(strcmp(hpp.ward,"Ward9")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "GeneralWard") == 0)
+        {
             w9++;
-        else if(strcmp(hpp.ward,"Ward10")==0)
+        }
+        else if (strcmp(hpp[i].ward_name, "MaternityWard") == 0)
+        {
             w10++;
+        }
     }
-    printf("Total number of patients: %d\n", count);
+
     fclose(fp);
+    printf("Total number of patients in GynoWard: %d\n", w1);
+
+    //printf("Name \t Age \t Address \t Condition \t Ward Name \t Number of Patients\n");
+    /*for (i = 0; i < 24; i++)
+    {
+        if (strcmp(hpp[i].ward_name, "GynoWard") == 0)
+        {
+
+            printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w1);
+        }
+    }
+        */
+    printf("Total number of patients in OrthoWard: %d\n", w2);
+    /*for (i = 0; i < 24; i++)
+    {
+        if (strcmp(hpp[i].ward_name, "OrthoWard") == 0)
+        {
+
+            printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w2);
+        }
+    }
+        */
+    printf("Total number of patients in CardioWard: %d\n", w3);
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "CardioWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w3);
+    //     }
+    // }
+    printf("Total number of patients in NeuroWard: %d\n", w4);
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "NeuroWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w4);
+    //     }
+    // }
+    printf("Total number of patients in PediatricWard: %d\n", w5);
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "PediatricWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w5);
+    //     }
+    // }
+    printf("Total number of patients in EmergencyWard: %d\n", w6);
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "EmergencyWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w6);
+    //     }
+    // }
+    printf("Total number of patients in ICUWard: %d\n", w7);
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "ICUWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w7);
+    //     }
+    // }
+    printf("Total number of patients in SurgicalWard: %d\n", w8);
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "SurgicalWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w8);
+    //     }
+    // }
+    printf("Total number of patients in GeneralWard: %d\n", w9);
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "GeneralWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w9);
+    //     }
+    // }
+    printf("Total number of patients in MaternityWard: %d\n", w10);
+
+    // for (i = 0; i < 24; i++)
+    // {
+    //     if (strcmp(hpp[i].ward_name, "MaternityWard") == 0)
+    //     {
+
+    //         printf("%s \t %d \t %s \t %s \t %s \t %d\n", hpp[i].name, hpp[i].age, hpp[i].address, hpp[i].condition, hpp[i].ward_name, w10);
+    //     }
+    // }
+
     return 0;
 }
+//The reason behind commit is the qn asked to count the number of patients in each ward but the code gives the full info of the patients in each ward. So I have commented the code which gives the full info of the patients in each ward and added the code which gives only the count of patients in each ward.
