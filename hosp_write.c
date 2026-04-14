e7f61d1116521100c31c7b3bc11cd491f9122409
Arun <arunbohara20641213@gmail.com>
2026-04-14 23:14:01 +0545

    Refactor patient data input and file writing

--- hosp_write.c
+++ hosp_write.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 struct hosp_write
 {
     char name[20];
@@ -7,23 +8,47 @@ struct hosp_write
     char condition[100];
     char ward[20];
 };
-int main(){
+
+int main()
+{
     struct hosp_write hpp;
     int i;
     FILE *fp;
-    fp=fopen("hospital.txt","w");
-    if(fp==NULL)
+
+    fp = fopen("hospital.txt", "w");
+    if(fp == NULL)
     {
         printf("Error opening file!");
         return 1;
     }
+
     printf("Enter the Details of 24 Patients:\n");
-    printf("Name \t Age \t Address \t Condition \t Ward\n");
-    for(i=1;i<=24;i++)
+    printf("Name | Age | Address | Condition | Ward\n");
+
+    for(i = 1; i <= 24; i++)
     {
-        scanf("%s %d %s %s %s",hpp.name,&hpp.age,hpp.address,hpp.condition,hpp.ward);
-        fprintf(fp,"%s\t%d\t%s\t%s\t%s\n",hpp.name,hpp.age,hpp.address,hpp.condition,hpp.ward);
+        printf("\nPatient %d:\n", i);
+
+        printf("Name: ");
+        scanf("%19s", hpp.name);
+
+        printf("Age: ");
+        scanf("%d", &hpp.age);
+
+        printf("Address (no spaces): ");
+        scanf("%49s", hpp.address); 
+
+        printf("Condition (no spaces): ");
+        scanf("%99s", hpp.condition);
+
+        printf("Ward: ");
+        scanf("%19s", hpp.ward);
+        fprintf(fp, "%s %d %s %s %s\n",
+                hpp.name, hpp.age, hpp.address, hpp.condition, hpp.ward);
     }
+
+    printf("\nData saved successfully to hospital.txt\n");
+
     fclose(fp);
     return 0;
 }
