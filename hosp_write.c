a1b152af0b75b10088d3d598fcc60fbddb4c24e3
Arun Rizzjew <arunbohara20641213@gmail.com>
2026-04-16 17:38:39 +0545

    Update hosp_write.c

--- hosp_write.c
+++ hosp_write.c
@@ -22,29 +22,30 @@ int main()
         return 1;
     }
 
-    printf("Enter the Details of 24 Patients:\n");
-    printf("Name | Age | Address | Condition | Ward\n");
+    printf("Enter details of 24 patients (NO spaces in input)\n");
 
-    for(i = 1; i <= 24; i++)
+    for(i = 0; i < 24; i++)
     {
-        printf("\nPatient %d:\n", i);
+        printf("\nPatient %d\n", i + 1);
 
         printf("Name: ");
-        scanf("%19s", hpp.name);
+        scanf("%s", hpp.name);
 
         printf("Age: ");
         scanf("%d", &hpp.age);
 
-        printf("Address (no spaces): ");
-        scanf("%49s", hpp.address); 
+        printf("Address: ");
+        scanf("%s", hpp.address);
 
-        printf("Condition (no spaces): ");
-        scanf("%99s", hpp.condition);
+        printf("Condition: ");
+        scanf("%s", hpp.condition);
 
         printf("Ward: ");
-        scanf("%19s", hpp.ward);
+        scanf("%s", hpp.ward);
+
         fprintf(fp, "%s %d %s %s %s\n",
-                hpp.name, hpp.age, hpp.address, hpp.condition, hpp.ward);
+                hpp.name, hpp.age, hpp.address,
+                hpp.condition, hpp.ward);
     }
 
     printf("\nData saved successfully to hospital.txt\n");
